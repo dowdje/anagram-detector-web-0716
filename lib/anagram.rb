29f704d97f90af2623dcb05e47f0ece6f653cc97
Jacob Edgerly-Dowd <dowdje89@gmail.com>
2016-07-27 23:16:36 +0000

    Dominated anagram lab: git it

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    @word_array = word_array
+    anagram = word_array.select {|x| x.split("").sort == @word.split("").sort}
+
+    anagram
+  end
+end
